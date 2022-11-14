acfa743abcadfad14510db0a7383ac69dae3723b
kovalevvova <kovalev.vova2013@gmail.com>
2022-11-14 13:47:46 +0300

    12

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-console.log(document.querySelector("title"));
+let burger = document.querySelector(".burger");
+
+burger.addEventListener("click", function () {
+    document.querySelector(".burger").classList.toggle("active")
+    document.querySelector(".nav-bar").classList.toggle("active")
+    document.querySelector("body").classList.toggle("active")
+    document.querySelector(".lang").classList.toggle("active")
+    document.querySelector(".header").classList.toggle("active")
+})
