3dcfc25d9cab8641465dd2c98ba819825a239998
kovalevvova <kovalev.vova2013@gmail.com>
2022-11-19 23:21:05 +0300

    444

--- js/index.js
+++ js/index.js
@@ -11,6 +11,11 @@ burger.addEventListener("click", function () {
 const anchors = document.querySelectorAll("a.item");
 for (let anchor of anchors) {
   anchor.addEventListener("click", function (e) {
+    document.querySelector(".burger").classList.toggle("active");
+    document.querySelector(".nav-bar").classList.toggle("active");
+    document.querySelector("body").classList.toggle("active");
+    document.querySelector(".lang").classList.toggle("active");
+    document.querySelector(".header").classList.toggle("active");
     e.preventDefault();
 
     let blockID = anchor.getAttribute("href");
