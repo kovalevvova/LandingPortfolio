c73089e7e48d3f921ef4b6d39c055562cc4a9b6d
kovalevvova <kovalev.vova2013@gmail.com>
2022-11-19 21:00:02 +0300

    4

--- js/index.js
+++ js/index.js
@@ -1,9 +1,23 @@
 let burger = document.querySelector(".burger");
 
 burger.addEventListener("click", function () {
-    document.querySelector(".burger").classList.toggle("active")
-    document.querySelector(".nav-bar").classList.toggle("active")
-    document.querySelector("body").classList.toggle("active")
-    document.querySelector(".lang").classList.toggle("active")
-    document.querySelector(".header").classList.toggle("active")
-})
+  document.querySelector(".burger").classList.toggle("active");
+  document.querySelector(".nav-bar").classList.toggle("active");
+  document.querySelector("body").classList.toggle("active");
+  document.querySelector(".lang").classList.toggle("active");
+  document.querySelector(".header").classList.toggle("active");
+});
+
+const anchors = document.querySelectorAll("a.item");
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    let blockID = anchor.getAttribute("href");
+
+    document.querySelector(blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+}
